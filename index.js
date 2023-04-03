bd92c8562ab8216276732f38e3b09303662a67e9
Ashay Morwale <ashay123.m@gmail.com>
2023-04-03 18:47:48 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+     cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+     cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    return [...cats.slice(0), name];
+}
+
+function prependCat(name) {
+    const newprependCat = [...cats];
+    newprependCat.unshift(name);
+    return newprependCat;
+}
+function removeLastCat() {
+    const newprependCat = [...cats];
+    newprependCat.pop();
+    return newprependCat;
+}
+
+function removeFirstCat() {
+    const newprependCat = [...cats];
+    newprependCat.shift();
+    return newprependCat;
+}
+
